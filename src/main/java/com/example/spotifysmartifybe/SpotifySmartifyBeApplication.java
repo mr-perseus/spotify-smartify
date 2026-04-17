43c7acf4085c5bf671230ee70e918e643219bafa
Jan <janfrei01@gmail.com>
2026-04-17 10:00:27 +0200

    Initial commit

---
+++ src/main/java/com/example/spotifysmartifybe/SpotifySmartifyBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifysmartifybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifySmartifyBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpotifySmartifyBeApplication.class, args);
+  }
+
+}
